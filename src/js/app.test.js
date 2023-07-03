2afbcce027541f50378511fe53218cd62a608e11
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-07-03 14:21:24 +0300

    error fixed

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -2,7 +2,7 @@ import demo from "./app";
 
 describe("Пример теста", () => {
   test.each([
-    { str: "Hello1!", expected: "Demo: Hello!" },
+    { str: "Hello!", expected: "Demo: Hello!" },
     { str: "", expected: "Demo: " },
     { str: 100, expected: "Demo: 100" },
   ])("demo($str)", ({ str, expected }) => {
