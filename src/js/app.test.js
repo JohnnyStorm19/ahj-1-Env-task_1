353b9ba7436397c2a3d31362e17f76ee5ff176c8
JohnnyStorm19 <112576084+JohnnyStorm19@users.noreply.github.com>
2023-07-03 14:16:14 +0300

    error added

--- src/js/app.test.js
+++ src/js/app.test.js
@@ -2,7 +2,7 @@ import demo from "./app";
 
 describe("Пример теста", () => {
   test.each([
-    { str: "Hello!", expected: "Demo: Hello!" },
+    { str: "Hello1!", expected: "Demo: Hello!" },
     { str: "", expected: "Demo: " },
     { str: 100, expected: "Demo: 100" },
   ])("demo($str)", ({ str, expected }) => {
